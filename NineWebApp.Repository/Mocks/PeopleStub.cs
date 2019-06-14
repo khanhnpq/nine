db0bc808844aff7b8bbdee54c92bb1a62fa1e981
Khanh Nguyen <kenngn@hotmail.com>
2019-06-14 16:34:58 +1000

    Fixed stub

--- NineWebApp.Repository/Mocks/PeopleStub.cs
+++ NineWebApp.Repository/Mocks/PeopleStub.cs
@@ -48,7 +48,7 @@ namespace NineWebApp.Repository.Mocks
 
                 if (race is Enums.Race.Hawaiian)
                 {
-                    people.Add(new Jute()
+                    people.Add(new Hawaiian()
                     {
                         Name = $@"Person #{i} is {Enums.Race.Hawaiian.ToString()}",
                         Age = ageRandom.Next(1, 99)
@@ -62,19 +62,6 @@ namespace NineWebApp.Repository.Mocks
                     
                 //});
             }
-            return people;
-        }
-
-        public static List<Person> InitPeople1()
-        {
-            List<Person> people = new List<Person>();
-
-            people.Add(new Person() { Name = "Person #1", Age = 25, Height = 1.7 });
-            people.Add(new Angle() { Name = "Person #2 Angle", Age = 26, Height = 1.8 });
-            people.Add(new Saxon() { Name = "Person #3 Saxon", Age = 27, Height = 1.9 });
-            people.Add(new Jute() { Name = "Person #4 Jute", Age = 28, Height = 1.6 });
-            people.Add(new Hawaiian() { Name = "Person #5 Hawaiian", Age = 29, Height = 1.5 });
-
             return people;
         }
     }
